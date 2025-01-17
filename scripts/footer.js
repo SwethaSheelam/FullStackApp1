a37a363e58f712274a33fb920644b9826f86afeb
sheelamswetha <sheelamswetha5@gmail.com>
2025-01-18 00:01:02 +0530

    Todo layout created

---
+++ scripts/footer.js
@@ -0,0 +1,10 @@
+const footer = () => {
+    let card =`
+         <div>
+        <h3>Made With Love &#10084;  <span>&copy; All Rights Reserved 2025</span> </h3>
+        
+    </div>
+    </div>`;
+   document.getElementById("footer").innerHTML= card
+};
+footer()
